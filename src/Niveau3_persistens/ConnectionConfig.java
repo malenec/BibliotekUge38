07e9583d2f5a7f867f5b7b2973a2ac3a715f330a
Mark Pedersen <sutsko@gmail.com>
2022-09-20 20:41:17 +0200

    connection switch

--- src/Niveau3_persistens/ConnectionConfig.java
+++ src/Niveau3_persistens/ConnectionConfig.java
@@ -6,22 +6,63 @@ import java.sql.DriverManager;
 public class ConnectionConfig {
 
     public static Connection getConnection() {
+
         Connection connection = null;
-        String url = "jdbc:mysql://localhost:3306/Bibliotek?serverTimezone=CET&useSSL=false";
+        String url = "jdbc:mysql://localhost:3306/Bibliotek?serverTimezone=CET&allowPublicKeyRetrieval=true&useSSL=false";
         String user = "root";
-        String password = null;
-        try {
-            password = "elvinha-17";
-        } catch (Exception e) {
-            password = "Grycecilie0510";
+        String password;
+        //
+
+
+
+
+            //humlen ved skiftet, prøv gerne at ændre så "elvinha-17" står i stedet for mit password begge steder.
+            if(connSwitch("Grycecilie0510")){
+                password = "Grycecilie0510";
+            } else {
+                password = "elvinha-17";
+            }
+
+
+
+
+
+
+            try {
+
+                connection = DriverManager.getConnection(url, user, password);
+
+            } catch (Exception e) {
+
+                e.printStackTrace();
+
+            }
+
+            return connection;
+
         }
-        try {
-            connection = DriverManager.getConnection(url, user, password);
-        } catch (Exception e) {
-            e.printStackTrace();
+
+        public static boolean connSwitch(String pass){
+
+            Connection connection = null;
+            String url = "jdbc:mysql://localhost:3306/Bibliotek?serverTimezone=CET&allowPublicKeyRetrieval=true&useSSL=false";
+            String user = "root";
+
+            String password = pass;
+
+
+            try {
+
+                connection = DriverManager.getConnection(url, user, password);
+
+            } catch (Exception e) {
+
+                return false;
+            }
+
+            return true;
         }
-        return connection;
-    }
+
 }
 
 
