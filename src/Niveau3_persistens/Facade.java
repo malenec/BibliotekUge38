305ac21ca06e8ae85332c8b99dc53111d53b977e
malenec <90346924+malenec@users.noreply.github.com>
2022-09-20 09:39:46 +0200

    konstruktion af mapper og facade påbegyndes

--- src/Niveau3_persistens/Facade.java
+++ src/Niveau3_persistens/Facade.java
@@ -1,4 +1,35 @@
 package Niveau3_persistens;
 
 public class Facade {
+
+    public static void opretLåner(){
+        Mapper.opretLåner();
+    }
+
+    public static void sletLåner(){
+        Mapper.sletLåner();
+    }
+
+    public static void opretBog(){
+        Mapper.opretBog();
+    }
+
+    public static void sletBog(){
+        Mapper.sletBog();
+    }
+
+    public static void opretUdlån(){
+        Mapper.opretUdlån();
+    }
+
+
+    static void sletUdlån(){
+        Mapper.sletUdlån();
+    }
+
+    static void søgIUdlån(){
+        Mapper.søgIUdlån();
+    }
+
+
 }
--- src/Niveau3_persistens/Mapper.java
+++ src/Niveau3_persistens/Mapper.java
@@ -2,4 +2,33 @@ package Niveau3_persistens;
 
 public class Mapper {
 
+    static void opretLåner(){
+
+    }
+
+    static void sletLåner(){
+
+    }
+
+    static void opretBog(){
+
+    }
+
+    static void sletBog(){
+
+    }
+
+    static void opretUdlån(){
+
+    }
+
+
+    static void sletUdlån(){
+
+    }
+
+    static void søgIUdlån(){
+
+    }
+
 }
